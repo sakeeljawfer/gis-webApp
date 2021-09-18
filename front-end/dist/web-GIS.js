990f92806b55f4bac7c4e353cf63bc761e003d65
HAJISTHA ASF <hajistha@quadrate.lk>
2021-09-18 17:17:35 +0530

    Bootstrap with Leaflet

---
+++ front-end/dist/web-GIS.js
@@ -0,0 +1,35 @@
+//Full screen map view
+var mapId = document.getElementById('map');
+function fullScreenView() {
+    if (document.fullscreenElement) {
+        document.exitFullscreen()
+    } else {
+        mapId.requestFullscreen();
+    }
+}
+
+//Print Map
+//$('.print-map').click(function(){
+   // window.print();
+//});
+
+//leaflet browser print function
+L.control.browserPrint({ position:'topright'}).addTo(map);
+
+
+//Leaflet Search Location
+L.Control.geocoder().addTo(map);
+
+//Leaflet Measure
+L.control.measure({
+    primaryLengthUnit: 'kilometers',
+    secondaryLengthUnit: 'meter',
+    primaryAreaUnit: 'sqmeters',
+    secondaryAreaUnit: undefined
+}).addTo(map);
+
+
+//zom to layer 
+$('.zoom-to-layer').click(function(){
+    map.setView([8.5025, 81.1804], 14)
+   });
